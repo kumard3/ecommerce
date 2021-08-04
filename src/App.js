974e1fdd7a1a385a185df5c9c2f0ccbe10fa4d8c
kumard3 <kumardeepanshu157200@gmail.com>
2021-08-04 18:48:36 +0530

    HomePage-done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// layouts
+import MainLayout from './layouts/MainLayout';
+// import HomepageLayout from './layouts/HomepageLayout';
+import HomepageLayout  from './layouts/HomePageLayout'
+// pages
+import HomePage from './pages/HomePage';
+import Registration from './pages/Registration';
+import './default.scss';
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
+      <Switch>
+        <Route exact path="/" render={() => (
+          <HomepageLayout >
+            <HomePage />
+          </HomepageLayout>
+        )} />
+        <Route path="/registration" render={() => (
+          <MainLayout >
+            <Registration />
+          </MainLayout>
+        )} />
+      </Switch>
     </div>
   );
 }
